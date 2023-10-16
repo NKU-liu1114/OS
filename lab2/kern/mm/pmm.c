3a74d4b2baff7c42475bb5bab25ff1e9d6ed8542
Sakura <129757875+NKU-liu1114@users.noreply.github.com>
2023-10-16 08:20:43 +0800

    Lab2

---
+++ lab2/kern/mm/pmm.c
@@ -0,0 +1,156 @@
+#include <default_pmm.h>
+#include <best_fit_pmm.h>
+#include <defs.h>
+#include <error.h>
+#include <memlayout.h>
+#include <mmu.h>
+#include <pmm.h>
+#include <sbi.h>
+#include <stdio.h>
+#include <string.h>
+#include <../sync/sync.h>
+#include <riscv.h>
+
+// virtual address of physical page array
+// pages指针保存的是第一个Page结构体所在的位置，也可以认为是Page结构体组成的数组的开头
+// 由于C语言的特性，可以把pages作为数组名使用，pages[i]表示顺序排列的第i个结构体
+struct Page *pages;
+// amount of physical memory (in pages)
+size_t npage = 0;
+// the kernel image is mapped at VA=KERNBASE and PA=info.base
+uint64_t va_pa_offset;
+// memory starts at 0x80000000 in RISC-V
+// DRAM_BASE defined in riscv.h as 0x80000000
+const size_t nbase = DRAM_BASE / PGSIZE;
+//(npage - nbase)表示物理内存的页数
+
+// virtual address of boot-time page directory
+uintptr_t *satp_virtual = NULL;
+// physical address of boot-time page directory
+uintptr_t satp_physical;
+
+// physical memory management
+const struct pmm_manager *pmm_manager;
+
+
+static void check_alloc_page(void);
+
+// init_pmm_manager - initialize a pmm_manager instance
+static void init_pmm_manager(void) {
+    pmm_manager = &best_fit_pmm_manager;
+    cprintf("memory management: %s\n", pmm_manager->name);
+    pmm_manager->init();
+}
+
+// init_memmap - call pmm->init_memmap to build Page struct for free memory
+static void init_memmap(struct Page *base, size_t n) {
+    pmm_manager->init_memmap(base, n);
+}
+
+// alloc_pages - call pmm->alloc_pages to allocate a continuous n*PAGESIZE
+// memory
+struct Page *alloc_pages(size_t n) {
+    struct Page *page = NULL;
+    bool intr_flag;
+    local_intr_save(intr_flag);
+    {
+        page = pmm_manager->alloc_pages(n);
+    }
+    local_intr_restore(intr_flag);
+    return page;
+}
+
+// free_pages - call pmm->free_pages to free a continuous n*PAGESIZE memory
+void free_pages(struct Page *base, size_t n) {
+    bool intr_flag;
+    local_intr_save(intr_flag);
+    {
+        pmm_manager->free_pages(base, n);
+    }
+    local_intr_restore(intr_flag);
+}
+
+// nr_free_pages - call pmm->nr_free_pages to get the size (nr*PAGESIZE)
+// of current free memory
+size_t nr_free_pages(void) {
+    size_t ret;
+    bool intr_flag;
+    local_intr_save(intr_flag);
+    {
+        ret = pmm_manager->nr_free_pages();
+    }
+    local_intr_restore(intr_flag);
+    return ret;
+}
+
+static void page_init(void) {
+    va_pa_offset = PHYSICAL_MEMORY_OFFSET;      //硬编码 0xFFFFFFFF40000000
+
+    uint64_t mem_begin = KERNEL_BEGIN_PADDR;    //硬编码 0x80200000
+    uint64_t mem_size = PHYSICAL_MEMORY_END - KERNEL_BEGIN_PADDR; 
+    // mem_size一部分用来放内核代码和数据，一部分用来存其他的东西
+    uint64_t mem_end = PHYSICAL_MEMORY_END; //硬编码取代 sbi_query_memory()接口
+
+    cprintf("physcial memory map:\n");
+    cprintf("  memory: 0x%016lx, [0x%016lx, 0x%016lx].\n", mem_size, mem_begin,
+            mem_end - 1);
+
+    uint64_t maxpa = mem_end;
+    // KERNTOP:0x88000000对应的虚拟地址,即最大物理地址对应的虚拟地址
+    if (maxpa > KERNTOP) {
+        maxpa = KERNTOP;
+    }
+    // end为内核代码和数据结束的虚拟地址
+    extern char end[];
+
+    npage = maxpa / PGSIZE;
+    // 对剩下的内存进行分页
+    // kernel在end[]结束, pages是剩下的页的开始
+    pages = (struct Page *)ROUNDUP((void *)end, PGSIZE);
+    // 把pages指针指向内核所占内存空间结束后的第一页
+
+    // 一开始把所有页面都设置为保留给内核使用的，之后再设置哪些页面可以分配给其他程序
+    for (size_t i = 0; i < npage - nbase; i++) {// npage - nbase 即是内核结束的虚拟地址到物理内存结束的虚拟地址之间分页的页数
+        SetPageReserved(pages + i);
+    }
+    // Page结构体在内存里排列在内核后面，这要占用一些内存。
+
+
+    // 从这个地方开始才是我们可以自由使用的物理内存
+    uintptr_t freemem = PADDR((uintptr_t)pages + sizeof(struct Page) * (npage - nbase));
+    // pages-freemem之间的内存存储页表，freemem之后的内存可自由使用
+    mem_begin = ROUNDUP(freemem, PGSIZE);
+    mem_end = ROUNDDOWN(mem_end, PGSIZE);
+    if (freemem < mem_end) {
+        //初始化我们可以自由使用的物理内存
+        // pa2page:抹去右边十二位，返回的是页号
+        init_memmap(pa2page(mem_begin), (mem_end - mem_begin) / PGSIZE);
+    }
+}
+
+/* pmm_init - initialize the physical memory management */
+void pmm_init(void) {
+    // We need to alloc/free the physical memory (granularity is 4KB or other size).
+    // So a framework of physical memory manager (struct pmm_manager)is defined in pmm.h
+    // First we should init a physical memory manager(pmm) based on the framework.
+    // Then pmm can alloc/free the physical memory.
+    // Now the first_fit/best_fit/worst_fit/buddy_system pmm are available.
+    init_pmm_manager();
+
+    // detect physical memory space, reserve already used memory,
+    // then use pmm->init_memmap to create free page list
+    page_init();
+
+    // use pmm->check to verify the correctness of the alloc/free function in a pmm
+    check_alloc_page();
+
+    extern char boot_page_table_sv39[];
+    satp_virtual = (pte_t*)boot_page_table_sv39;
+    satp_physical = PADDR(satp_virtual);
+    cprintf("satp virtual address: 0x%016lx\nsatp physical address: 0x%016lx\n", satp_virtual, satp_physical);
+}
+
+static void check_alloc_page(void) {
+    pmm_manager->check();
+    cprintf("check_alloc_page() succeeded!\n");
+}
